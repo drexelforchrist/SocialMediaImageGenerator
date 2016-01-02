91759f5f4d3f32ca01cb97ea7d4393e38673ca2d
James Kurtz <jkrrv@jkrrv.org>
2016-01-01 19:07:40 -0500

    Now using DML instead of DB queries Allows images to be generated for all pages, rather than just events.

--- facebook.php
+++ facebook.php
@@ -1,39 +1,34 @@
 <?php
 
-// Open Database Connection
-$db = new PDO("mysql:host=brilliant-core.c4z81yuf0edn.us-east-1.rds.amazonaws.com:3306;dbname=drexelforchrist;charset=utf8","drexelforc_rApp","jaAMamGFKr8zpvY5");
+$dml = new SimpleXMLElement(file_get_contents('http://dev.drexelforchrist.org/'.$_SERVER['PATH_INFO'].'.dml'));
 
-// Query the database
-$stmt = $db->prepare("SELECT name, subtitle, parent, calendarID FROM drexelforchrist.events WHERE eventId=?");
-$stmt->execute(array(intval($_GET['eid'])));
-$eventInfo = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$lastEid = intval($_GET['eid']);
+$title = $dml->document->title;
 
-while(($eventInfo['name']==null || $eventInfo['subtitle']==null) && $lastEid != $eventInfo['calendarID']) {
+try {
+	$subtitle = $dml->document->subtitle;
+} catch (Exception $e) {
+	$subtitle = null;
+}
 
-	$stmt->execute(array(intval($eventInfo['parent'])));
-	$lastEid = intval($eventInfo['parent']);
-	$parentInfo = $stmt->fetch(PDO::FETCH_ASSOC);
-	$eventInfo['name'] = $eventInfo['name'] ?: $parentInfo['name'];
-	$eventInfo['subtitle'] = $eventInfo['subtitle'] ?: $parentInfo['subtitle'];
-
-	$eventInfo['parent'] = $parentInfo['parent'];
+try {
+	$template = $dml->document->template->name;
+} catch (Exception $e) {
+	$template = null;
 }
 
 //Set the Content Type
 header('Content-type: image/jpeg');
-header('x-event-name: ' . $eventInfo['calendarID']);
 
 // Create Image From Existing File
-switch($eventInfo['calendarID']) {
-	case 2: // church
+switch($template) {
+	case 'church': // church
 		$jpg_image = imagecreatefromjpeg('churchDefaultOpengraph.jpg');
 		break;
-	case 3: // prayer
+	case 'prayer': // prayer
 		$jpg_image = imagecreatefromjpeg('prayerDefaultOpengraph.jpg');
 		break;
-	case 5: // discipleship
+	case 'discipleship': // discipleship
 		$jpg_image = imagecreatefromjpeg('discipleshipDefaultOpengraph.jpg');
 		break;
 	default:
@@ -46,28 +41,26 @@ $white = imagecolorallocate($jpg_image, 255, 255, 255);
 // Set Path to Font File
 $font_path = 'Cabin-Regular-TTF.ttf';
 
-// Set Text to Be Printed On Image
-$text = $eventInfo['name'];
-
 // Print Text On Image
 
 $size = 100;
 do {
-	$corners = imagettfbbox ($size, 0, $font_path, $text);
+	$corners = imagettfbbox ($size, 0, $font_path, $title);
 } while ($corners[4] > 1080 && $size--);
 
-imagettftext($jpg_image, $size, 0, 30, 180, $white, $font_path, $text);
+imagettftext($jpg_image, $size, 0, 30, 180, $white, $font_path, $title);
 
 
 // subtitle, too.
- if (isset($eventInfo['subtitle']) && $eventInfo['subtitle']!="") {
+ if ($subtitle != null) {
 
-	// Set Text to Be Printed On Image
-	$text = $eventInfo['subtitle'];
+	 $size = min(45, $size); // subtitle shouldn't be bigger than the title
+	 do {
+		 $corners = imagettfbbox ($size, 0, $font_path, $subtitle);
+	 } while ($corners[4] > 1080 && $size--);
 
 	// Print Text On Image
-	imagettftext($jpg_image, 45, 0, 30, 270, $white, $font_path, $text);
-
+	imagettftext($jpg_image, $size, 0, 30, 270, $white, $font_path, $subtitle);
 }
 
 // Send Image to Browser
